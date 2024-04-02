35e066451645031550a479a606c86b98532f789e
lensil <147991201+lensil@users.noreply.github.com>
2024-04-02 20:24:25 +0200

    Updated DFA to be less verbose

--- Compiler/Lexer/dfa.py
+++ Compiler/Lexer/dfa.py
@@ -6,41 +6,7 @@ This file contains functionality related to the DFA that the lexer will use to t
 
 from enum import Enum
 
-class DFAStates(Enum):
-
-    """
-    
-    This class is an enumeration of all the states that the DFA can be in.
-    
-    """
-
-    S_0 = 0 # Start state
-    S_1 = 1
-    S_2 = 2
-    S_3 = 3
-    S_4 = 4
-    S_5 = 5
-    S_6 = 6
-    S_7 = 7
-    S_8 = 8
-    S_9 = 9
-    S_10 = 10
-    S_11 = 11
-    S_12 = 12
-    S_13 = 13
-    S_14 = 14
-    S_15 = 15
-    S_16 = 16
-    S_17 = 17
-    S_18 = 18
-    S_19 = 19
-    S_20 = 20
-    S_21 = 21
-    S_22 = 22
-    S_23 = 23
-    S_24 = 24
-    S_25 = 25
-    S_26 = 26
+DFAStates = Enum('DFAStates', {f'S_{i}': i for i in range(27)})
 
 def input_categories(character):
     
@@ -54,11 +20,11 @@ def input_categories(character):
         """
 
         match character:
-            case character.isdigit():
+            case char if char.isdigit():
                 return 'digit'
             case 'a' | 'A' | 'b' | 'B' | 'c' | 'C' | 'd' | 'D' | 'e' | 'E' | 'f' | 'F':
                 return 'hex'
-            case character.isalpha():
+            case char if char.isalpha():
                 return 'letter'
             case '+':
                 return 'plus'
@@ -90,16 +56,10 @@ def input_categories(character):
                 return 'underscore'
             case __:
                 return 'other'
-        
-print(input_categories('8')) # Should print 'digit'
-
 
 class DFA:
-
     """
-        
-        This class defines the DFA that the lexer will use to tokenize the input.
-        
+    This class defines the DFA that the lexer will use to tokenize the input.
     """
 
     def __init__(self):
@@ -110,7 +70,6 @@ class DFA:
         
         # Define the transitions for the DFA as a dictionary
         self.transitions = {
-
             # Tokenizing operators
             (self.states.S_0, 'minus'): self.states.S_1,
             (self.states.S_1, 'greater'): self.states.S_2,
@@ -126,61 +85,22 @@ class DFA:
 
             # Tokenizing comments
             (self.states.S_4, 'slash'): self.states.S_5,
-            (self.states.S_5, 'digit'): self.states.S_5,
-            (self.states.S_5, 'hex'): self.states.S_5,
-            (self.states.S_5, 'letter'): self.states.S_5,
-            (self.states.S_5, 'plus'): self.states.S_5,
-            (self.states.S_5, 'minus'): self.states.S_5,
-            (self.states.S_5, 'star'): self.states.S_5,
-            (self.states.S_5, 'slash'): self.states.S_5,
-            (self.states.S_5, 'punctuation'): self.states.S_5,
-            (self.states.S_5, 'whitespace'): self.states.S_5,
             (self.states.S_5, 'newline'): self.states.S_6,
-            (self.states.S_5, 'equals'): self.states.S_5,
-            (self.states.S_5, 'less'): self.states.S_5,
-            (self.states.S_5, 'greater'): self.states.S_5,
-            (self.states.S_5, 'exclamation'): self.states.S_5,
-            (self.states.S_5, 'hash'): self.states.S_5,
-            (self.states.S_5, 'dot'): self.states.S_5,
-            (self.states.S_5, 'underscore'): self.states.S_5,
-            (self.states.S_5, 'other'): self.states.S_5,
             (self.states.S_4, 'star'): self.states.S_7,
-            (self.states.S_7, 'digit'): self.states.S_7,
-            (self.states.S_7, 'hex'): self.states.S_7,
-            (self.states.S_7, 'letter'): self.states.S_7,
-            (self.states.S_7, 'plus'): self.states.S_7,
-            (self.states.S_7, 'minus'): self.states.S_7,
             (self.states.S_7, 'star'): self.states.S_8,
-            (self.states.S_7, 'slash'): self.states.S_7,
-            (self.states.S_7, 'punctuation'): self.states.S_7,
-            (self.states.S_7, 'whitespace'): self.states.S_7,
-            (self.states.S_7, 'newline'): self.states.S_7,
-            (self.states.S_7, 'equals'): self.states.S_7,
-            (self.states.S_7, 'less'): self.states.S_7,
-            (self.states.S_7, 'greater'): self.states.S_7,
-            (self.states.S_7, 'exclamation'): self.states.S_7,
-            (self.states.S_7, 'hash'): self.states.S_7,
-            (self.states.S_7, 'dot'): self.states.S_7,
-            (self.states.S_7, 'underscore'): self.states.S_7,
-            (self.states.S_7, 'other'): self.states.S_7,
-            (self.states.S_8, 'digit'): self.states.S_7,
-            (self.states.S_8, 'hex'): self.states.S_7,
-            (self.states.S_8, 'letter'): self.states.S_7,
-            (self.states.S_8, 'plus'): self.states.S_7,
-            (self.states.S_8, 'minus'): self.states.S_7,
-            (self.states.S_8, 'star'): self.states.S_8,
-            (self.states.S_8, 'slash'): self.states.S_6,
-            (self.states.S_8, 'punctuation'): self.states.S_7,
-            (self.states.S_8, 'whitespace'): self.states.S_7,
-            (self.states.S_8, 'newline'): self.states.S_7,
-            (self.states.S_8, 'equals'): self.states.S_7,
-            (self.states.S_8, 'less'): self.states.S_7,
-            (self.states.S_8, 'greater'): self.states.S_7,
-            (self.states.S_8, 'exclamation'): self.states.S_7,
-            (self.states.S_8, 'hash'): self.states.S_7,
-            (self.states.S_8, 'dot'): self.states.S_7,
-            (self.states.S_8, 'underscore'): self.states.S_7,
-            (self.states.S_8, 'other'): self.states.S_7,
+            (self.states.S_8, 'star'): self.states.S_8
+        }
+        
+        for char in ['digit', 'hex', 'letter', 'plus', 'minus', 'star', 'slash', 'punctuation', 'whitespace', 'equals', 'less', 'greater', 'exclamation', 'hash', 'dot', 'underscore', 'other']:
+            self.transitions.update({(self.states.S_5, char): self.states.S_5})
+            
+        for char in ['digit', 'hex', 'letter', 'plus', 'minus', 'slash', 'punctuation', 'whitespace', 'newline', 'equals', 'less', 'greater', 'exclamation', 'hash', 'dot', 'underscore', 'other']:
+            self.transitions.update({(self.states.S_7, char): self.states.S_7})
+
+        for char in ['digit', 'hex', 'letter', 'plus', 'minus', 'slash', 'punctuation', 'whitespace', 'newline', 'equals', 'less', 'greater', 'exclamation', 'hash', 'dot', 'underscore', 'other']:
+            self.transitions.update({(self.states.S_8, char): self.states.S_7})
+
+        self.transitions.update({
 
             # Tokenizing the assignment operator
             (self.states.S_0, 'equals'): self.states.S_9,
@@ -226,5 +146,18 @@ class DFA:
             (self.states.S_23, 'hex'): self.states.S_24,
             (self.states.S_24, 'hex'): self.states.S_25,
             (self.states.S_25, 'hex'): self.states.S_26
+        })
 
-        }
+    def accepting_states(self, state):
+        
+        """
+            
+            This function checks if the given state is an accepting state.
+    
+            Parameters:
+                state (DFAStates): The state to check.
+            
+            """
+    
+        return state in self.final_states
+
